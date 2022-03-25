f5629bb0fbcf18969e2005ce649bef7082822497
CeJ <1399651457@qq.com>
2022-03-25 15:49:53 +0800

    v0.1(tree, total: 9) Author: CeJ Complete: No Code Review: Myself

---
+++ tree/112/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "algorithm-go/tree"
+
+// 112. 路径总和
+
+func hasPathSum(root *tree.TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return root.Val == targetSum
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
