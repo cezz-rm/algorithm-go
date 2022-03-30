d45745303dbc2889f94a1995e8e63e246c1d67f3
CeJ <1399651457@qq.com>
2022-03-30 17:40:26 +0800

    v0.1(tree, total: 20) Author: CeJ Complete: No Code Review: Myself

---
+++ tree/617/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "algorithm-go/tree"
+
+// 617. 合并二叉树
+
+func mergeTrees(root1 *tree.TreeNode, root2 *tree.TreeNode) *tree.TreeNode {
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+	root1.Val += root2.Val
+	root1.Left = mergeTrees(root1.Left, root2.Left)
+	root1.Right = mergeTrees(root1.Right, root2.Right)
+	return root1
+}
